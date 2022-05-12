c8b00193042afd9b10681d51fef6f0cb81179165
humber-jake <humber.jake@gmail.com>
2022-05-11 17:20:44 -0700

    Implement countdown to tomorrow timer, pick word based on days since epoch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import GameBoard from './GameBoard.js'
 import GameEndDialog from './GameEndDialog.js'
+import PossibleAnswers from './PossibleAnswers.js'
+import PossibleGuesses from './PossibleGuesses.js'
+import shuffleSeed from 'shuffle-seed';
 
+
+function handleDate(){
+  
+  let today = new Date()
+  let tomorrow = new Date()
+  tomorrow.setDate(today.getDate() + 1)
+  tomorrow.setHours(0,0,0,0);
+  
+  let countdown = {
+      hours: Math.floor((tomorrow - today) / (1000 * 60 * 60)).toString().padStart(2,'0'),
+      minutes: Math.floor((tomorrow - today) % (1000 * 60 * 60) / (1000 * 60)).toString().padStart(2,'0'),
+      seconds: Math.floor((tomorrow - today) % (1000 * 60 * 60) / (1000) % 60).toString().padStart(2,'0'),
+    }
+
+    return countdown;
+}
+  
+function daysSinceEpoch(){
+    return Math.floor(new Date() / (1000 * 60 * 60 * 24))
+}
+  
+function getNewWord(){
+    let shuffledAnswers = shuffleSeed.shuffle(PossibleAnswers, 'seed')
+    let day = daysSinceEpoch() % shuffledAnswers.length;
+    console.log(day)
+    console.log(PossibleAnswers[day]);
+    console.log(shuffledAnswers[day]);
+    return shuffledAnswers[day];
+}
+  
+getNewWord();
+  
 function App() {
+      
+  const [timer, setTimer] = useState(handleDate());
+  
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      setTimer(handleDate());
+    }, 1000);
+    return () => clearInterval(timeout);
+  });
+
+
   return (
     <div className="App">
+      <h1 style={{textAlign: 'center'}}>{timer.hours}: {timer.minutes}: {timer.seconds}</h1>
         <GameBoard answer='bogus' maxAttempts={6}/>
         <GameEndDialog />
     </div>
